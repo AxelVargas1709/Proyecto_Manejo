99928ed57d032620ae4e0bcb5ab6c055047d404d
Axel <axelvargasvilla@hotmail.com>
2022-01-21 20:34:07 -0500

    Creacion del Proyecto

---
+++ PM2022/src/pm2022/PM2022.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pm2022;
+
+/**
+ *
+ * @author Axel Vargas
+ */
+public class PM2022 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
